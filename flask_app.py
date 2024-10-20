5458b38e1fd0ad31690dd4ff711d9988602eda6f
solstxce <48374053+solstxce@users.noreply.github.com>
2024-10-21 02:00:37 +0530

    updatedCORS

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -6,6 +7,7 @@ from PIL import Image
 import io
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
 # Load the model
 model = tf.keras.models.load_model('Pesticide_Sprayer.h5')
